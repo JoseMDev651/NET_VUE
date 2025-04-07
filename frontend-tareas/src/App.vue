52e4fdc655bd9b60a7fbc56ae3334d60bc54d4d7
Jose <jmlopez651@outlook.com>
2025-04-07 11:58:13 +0200

    Install Bootstrap, Create component

--- frontend-tareas/src/App.vue
+++ frontend-tareas/src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Hola Mundo"/>
+  <MyTarea />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyTarea from './components/Tarea.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyTarea
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
