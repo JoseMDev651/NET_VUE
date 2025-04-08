ed50bd772e133805b20103c6160cddfe15786c32
Jose <jmlopez651@outlook.com>
2025-04-08 12:01:09 +0200

    Add elements to a list, Add fontawesome, Include circle icon in element, Include trash icon, Bootstrap, Add work to list on clik in button Add, Delete element of list, Edit list element(not finished)

--- frontend-tareas/src/components/Tarea.vue
+++ frontend-tareas/src/components/Tarea.vue
@@ -7,11 +7,27 @@
                 <div class="card mt-4">
                     <div class="card-body">
                         <div class="input-group">
-                            <input type="text" class="form-control form-control-lg" placeholder="Agregar tarea">
+                            <input type="text" v-model="tarea"
+                            class="form-control form-control-lg" placeholder="Agregar tarea">
                             <div class="input-group-append">
-                                <button class="btn btn-success btn-lg">Agregar</button>
+                                <button v-on:click="agregarTarea()"
+                                class="btn btn-success btn-lg">Agregar</button>
                             </div>
                         </div>
+                        <br>
+                        <h4 v-if="listTareas.length == 0">No hay tarea para realizar</h4>
+                        <ul class="list-group">
+                            <li v-for="(tarea,index) of listTareas" :key="index"
+                            class="list-group-item d-flex justify-content-between">
+                                <span class="cursor" v-on:click="editarTarea(tarea, index)">
+                                    <i class="fa-regular fa-circle"></i>
+                                </span>
+                                {{ tarea.nombre }}
+                                <span class="text-danger cursor" v-on:click="eliminarTarea(index)">
+                                    <i class="fa-solid fa-trash"></i>
+                                </span>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -22,9 +38,33 @@
 <script>
     export default {
         name:"MyTarea",
+        data(){
+            return{
+                tarea:'',
+                listTareas:[]
+            }
+        },
+        methods:{
+            agregarTarea(){
+               const tarea = {
+                nombre: this.tarea,
+                estado: false
+               }
+               this.listTareas.push(tarea);
+               this.tarea='';
+            },
+            eliminarTarea(index){
+                this.listTareas.splice(index,1);
+            },
+            editarTarea(tarea, index){
+                this.listTareas[index].estado = !tarea.estado
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+.cursor{
+    cursor: pointer;
+}
 </style>
