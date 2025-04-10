b3f39ebac06859b37488ae7323ecf82eae16cd7e
Jose <jmlopez651@outlook.com>
2025-04-10 12:25:17 +0200

    Create models, Controllers, Install dependencies, Create DBContext, appsetting connection string,programs.cs

---
+++ backend-tareas/Models/Tarea.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend_tareas.Models
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public required string Nombre { get; set; }
+        [Required]
+        public required bool Estado { get; set; }
+    }
+}
